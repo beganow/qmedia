07658563cb6a77d1a947b3b30d2a2a4b46d4f87d
Pavel Obodov <pashok12.obodov@gmail.com>
2024-09-13 22:40:01 +0300

    first

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+
+const server = express();
+const PORT = process.env.PORT || 3000;
+
+server.use(cors());
+server.use(bodyParser.json());
+server.use('/',express.static(path.join(__dirname, 'public')));
+
+
+server.post('/send-email', (req, res) => {
+    const { name, email, seminar } = req.body;
+
+    if (email === 'Pashok12.obodov@gmail.com') {
+        return res.status(403).send({ success: false, message: 'User is not authorized to send emails.' });
+    }
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'Pashok12.obodov@gmail.com',
+            pass: 'sdtbwxftlelzqtel',
+        },
+    });
+
+    const mailOptions = {
+        from: 'Pashok12.obodov@gmail.com',
+        to: email,
+        subject: 'Подтверждение регистрации на семинар',
+        text: `Здравствуйте, ${name}!\n\nВы успешно зарегистрированы на семинар:${seminar}.\n\nС уважением,\nКоманда семинара`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).send({ success: false, message: error.toString() });
+        }
+        res.send({ success: true, message: 'Email sent: ' + info.response });
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
