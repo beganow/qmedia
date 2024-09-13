8687ae7254a5c1d0e1cb71543b6baea8b8ad1408
Pavel Obodov <pashok12.obodov@gmail.com>
2024-09-13 23:19:46 +0300

    first

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ server.use(bodyParser.json());
 server.use('/',express.static(path.join(__dirname, 'public')));
 server.use(bodyParser.json());
 
-server.post('/submit', (req, res) => {
+server.post('https://qmedia-five.vercel.app/submit', (req, res) => {
     const { name, email, seminar } = req.body;
 
     if (email === 'Pashok12.obodov@gmail.com') {
