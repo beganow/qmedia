4fab515fa3354ec94e23fae8893403bc144dfe01
Pavel Obodov <pashok12.obodov@gmail.com>
2024-09-13 22:58:24 +0300

    first

--- public/index.js
+++ public/index.js
@@ -62,7 +62,7 @@ document.addEventListener('DOMContentLoaded', function() {
             seminar: seminar
         };
 
-        fetch('https://qmedia-five.vercel.app/send-email', {
+        fetch('https://qmedia-five.vercel.app', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
