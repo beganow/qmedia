36df0357bf49877adcb845a3722af00e1eb2fcc7
Pavel Obodov <pashok12.obodov@gmail.com>
2024-09-14 00:07:33 +0300

    first

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 server.use(cors());
 server.use(bodyParser.json());
-server.use('/',express.static(path.join(__dirname, 'public')));
+server.use(express.static(path.join(__dirname, 'public')));
 server.use(bodyParser.json());
 server.use(cors({
     origin: '*',
